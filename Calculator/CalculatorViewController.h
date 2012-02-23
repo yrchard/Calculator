60681fd9f33d88245a6ca2d756bc32fa4a975eb6
Richard Nutley <richard.nutley@gmail.com>
2012-02-23 14:19:51 -0800

    minor commit - added a comment

--- Calculator/CalculatorViewController.h
+++ Calculator/CalculatorViewController.h
@@ -6,6 +6,8 @@
 //  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
 //
 
+//  This comment added on 2/23/12 at 2:18pm.
+
 #import <UIKit/UIKit.h>
 
 @interface CalculatorViewController : UIViewController
